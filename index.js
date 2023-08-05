18ee4195e42864f4ba8ab4abc639ac30d9cb4fd4
germandeburzaco <germandeburzaco@hotmail.com>
2023-08-05 09:10:41 -0300

    motor de vistas

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express")
 
 const app = express()
 
+app.set('views', './src/views')
+app.set("view engine", "ejs")
+
 /*****VARIABLES******/
 const APP_PORT = process.env.PORT ?? "8080"
 
 app.get("/*", (req, res)=>{
-    res.send("HOLA MUNDO CRUEL")
+    res.render(index)
 })
 
 app.listen(APP_PORT, ()=>{
-    console.log("APP COORIENDO EN: " + APP_PORT)
+    console.log("APP CORRIENDO EN: " + APP_PORT)
 })
