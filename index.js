5e3fec16689e1d749c5126bb22b3b966e1a3dcfa
germandeburzaco <germandeburzaco@hotmail.com>
2023-08-05 09:17:53 -0300

    varias configuraciones

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express")
-
+const path =require("node:path")
+var bodyParser = require('body-parser')
 const app = express()
 
-app.set('views', './src/views')
+app.use(express.json());
+app.set('views', path.join(__dirname, '/src/views'))
 app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 /*****VARIABLES******/
 const APP_PORT = process.env.PORT ?? "8080"
 
 app.get("/*", (req, res)=>{
-    res.render(index)
+    res.render("index")
 })
 
 app.listen(APP_PORT, ()=>{
