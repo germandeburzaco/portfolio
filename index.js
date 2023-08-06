449e755ae20b8cb038780626ab63baec446df2a4
germandeburzaco <germandeburzaco@hotmail.com>
2023-08-06 08:51:08 -0300

    log de seguridad

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(bodyParser.json())
 /*****VARIABLES******/
 const APP_PORT = process.env.PORT ?? "8080"
 
+
+/******MIDDLEWARES********/
+app.use((req, res, next)=>{
+    console.log("-------------------INICIO----------------------------")
+    console.log("-----------------------------------------------------")
+    const d = new Date();
+    const dia = d.getDate()  
+    const mes = d.getMonth() + 1
+    console.log(d.getHours().toString().padStart(2, "0") + ":" + d.getMinutes().toString().padStart(2, "0") + ":" + d.getSeconds().toString().padStart(2, "0") +"  -  " + dia +"-" + mes +"-" + d.getFullYear()  )  
+    console.log("URL: " + req.method + " " + req.originalUrl)  
+    console.log("IP: " + req.ip)  
+    console.log("----------------------------------------------------")
+    console.log("-------------------FIN-------------------------------") 
+
+    next();
+})
+
 app.get("/", (req, res)=>{
     res.render("index")
 })
