f429d20f92d83cf20c056ad2b825a551618c3d55
German <52635113+germandeburzaco@users.noreply.github.com>
2024-11-13 13:13:58 -0300

    Update variables.js

--- helpers/variables.js
+++ helpers/variables.js
@@ -6,13 +6,13 @@ module.exports = {
             "id": 3394,
             "user_name": "GERMANG",
             "modo": "oscuro",
-            "ciudad": "germandeburzaco@hotmail.com"
+            "ciudad": "ge@hotmail.com"
           },
           {
             "id": 9794,
             "user_name": "MARIANAM",
             "modo": "oscuro",
-            "ciudad": "marianamolina@live.com"
+            "ciudad": "ma@live.com"
           }
     ]
 }
